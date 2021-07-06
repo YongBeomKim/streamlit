63231d63d2080841f7c5a5094ddd8de8ba9f1d1a
YongBeomKim <saltman21@naver.com>
2021-07-06 23:31:37 +0900

    part3 display Dataset

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ## Reference Sites
 - **[How to deploy Streamlit App on Heroku](https://medium.com/@harshvai07/deploying-streamlit-web-application-with-heroku-22c53332a41f)**
 - **[Markdown Cheat Sheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Here-Cheatsheet)**
+- **[Heroku 잠들지 마](https://nhj12311.tistory.com/283)**
+- **[Streamlit Multi Pages](https://towardsdatascience.com/creating-multipage-applications-using-streamlit-efficiently-b58a58134030)**
--- demo.py
+++ demo.py
@@ -1,14 +1,61 @@
 import streamlit as st
+import webbrowser
+from PIL import Image
+import pandas as pd
+import numpy as np
+import time
 
-# http://www.coffeesnoopers.com/contents/general/general.asp?page_str_menu=12
+logo_image = './media/logo.png'
+url = 'http://www.coffeesnoopers.com/contents/general/general.asp?page_str_menu=12'
+image = Image.open(logo_image)
 
-## HTML div Text Areas
-st.title("커피 스누퍼스 인공지능 모델링")
+# <Headers>
+st.title("")
+st.image(image, caption='과학적이고 정량적인 커피 분석을 향한 길')
+st.markdown("# **커피 스누퍼스 인공지능 모델링**")
+if st.button('홈페이지 바로가기'):
+    webbrowser.open_new_tab(url)
+# https://discuss.streamlit.io/t/how-to-link-a-button-to-a-webpage/1661/2
+# st.markdown("# **[커피 스누퍼스 인공지능 모델링]()**")
+# st.title("커피 스누퍼스 인공지능 모델링")
+# if st.checkbox('checkbox'):
+#     st.image(banner_image)
+
+## Chapter 3 Pandas Data
+a = [1, 2, 3, 4, 5, 6, 7, 8]
+n = np.array(a)
+nd = n.reshape((2, 4))
+dic = {"name": ["harsh", "Gupta"], "age": [21, 32], "city": ["noida", "delhi"]}
+data = pd.read_csv("media/Salary_Data.csv")
+
+# st.dataframe(data, width="100%", height=500)
+st.dataframe(data.head(5), width=600, height=500)
+st.table(data.head(3))
+st.table(dic)
+st.json(dic)
+st.write(dic)
+
+
+@st.cache
+def ret_time(a):
+    time.sleep(3)
+    return "모델 정확도 : 0.982142354"
+    # return time.time()
+
+
+if st.checkbox("1"):
+    st.write(ret_time(1))
+
+if st.checkbox("2"):
+    st.write(ret_time(2))
+
+## Chapter 1, 2 Display Texts
+# <Contents>
 st.header("Header")
 st.subheader("Sub Header")
 st.text("Link this video and subscribe to our site")
 
-## MarkDown Text Areas
+## <Contents> MarkDown Text
 st.markdown(
     """
 # h1 Tag
